b454b15959b97157293a060d9dde67714afa75b2
eyuboglu <eyuboglu@stanford.edu@cr-a100-80-0004.identity.together.xyz>
2023-11-06 18:41:09 +0000

    Add hedgehog

---
+++ zoology/mixers/hedgehog.py
@@ -0,0 +1,464 @@
+"""
+Linear attention feature maps (Hedgehog M. Zhang et al. 2023)
+"""
+import math
+import torch
+import copy
+import warnings
+import torch.nn as nn
+import torch.nn.functional as F
+import opt_einsum as oe
+from einops import rearrange
+
+from typing import List, Optional, Tuple, Union
+
+
+# Copyright 2023 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import List, Optional, Tuple, Union
+
+import torch
+
+
+class AttentionMaskConverter:
+    """
+    A utility attention mask class that allows one to:
+        - Create a causal 4d mask
+        - Create a causal 4d mask with slided window
+        - Convert a 2d attention mask (batch_size, query_length) to a 4d attention mask (batch_size, 1, query_length,
+          key_value_length) that can be multiplied with attention scores
+
+    Parameters:
+        is_causal (`bool`):
+            Whether the attention mask should be a uni-directional (causal) or bi-directional mask.
+
+        sliding_window (`int`, *optional*):
+            Optionally, the sliding window masks can be created if `sliding_window` is defined to a positive integer.
+    """
+
+    def __init__(self, is_causal: bool, sliding_window: Optional[int] = None):
+        self.is_causal = is_causal
+        self.sliding_window = sliding_window
+
+        if self.sliding_window is not None and self.sliding_window <= 0:
+            raise ValueError(
+                f"Make sure that when passing `sliding_window` that its value is a strictly positive integer, not `{self.sliding_window}`"
+            )
+
+    def to_causal_4d(
+        self,
+        batch_size: int,
+        query_length: int,
+        key_value_length: int,
+        dtype: torch.dtype = torch.float32,
+        device: Union[torch.device, "str"] = "cpu",
+    ) -> torch.Tensor:
+        """
+        Creates a causal 4D mask of (bsz, head_dim=1, query_length, key_value_length) shape and adds large negative
+        bias to upper right hand triangular matrix (causal mask).
+        """
+        if not self.is_causal:
+            raise ValueError(f"Please use `to_causal_4d` only if {self.__class__} has `is_causal` set to True.")
+
+        # If shape is not cached, create a new causal mask and cache it
+        input_shape = (batch_size, query_length)
+        past_key_values_length = key_value_length - query_length
+
+        # create causal mask
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        causal_4d_mask = None
+        if input_shape[-1] > 1 or self.sliding_window is not None:
+            causal_4d_mask = self._make_causal_mask(
+                input_shape,
+                dtype,
+                device=device,
+                past_key_values_length=past_key_values_length,
+                sliding_window=self.sliding_window,
+            )
+
+        return causal_4d_mask
+
+    def to_4d(
+        self,
+        attention_mask_2d: torch.Tensor,
+        query_length: int,
+        key_value_length: Optional[int] = None,
+        dtype: torch.dtype = torch.float32,
+    ) -> torch.Tensor:
+        """
+        Converts 2D attention mask to 4D attention mask by expanding mask to (bsz, head_dim=1, query_length,
+        key_value_length) shape and by adding a large negative bias to not-attended positions. If attention_mask is
+        causal, a causal mask will be added.
+        """
+        input_shape = (attention_mask_2d.shape[0], query_length)
+
+        # create causal mask
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        causal_4d_mask = None
+        if (input_shape[-1] > 1 or self.sliding_window is not None) and self.is_causal:
+            if key_value_length is None:
+                raise ValueError(
+                    "This attention mask converter is causal. Make sure to pass `key_value_length` to correctly create a causal mask."
+                )
+
+            past_key_values_length = key_value_length - query_length
+            causal_4d_mask = self._make_causal_mask(
+                input_shape,
+                dtype,
+                device=attention_mask_2d.device,
+                past_key_values_length=past_key_values_length,
+                sliding_window=self.sliding_window,
+            )
+        elif self.sliding_window is not None:
+            raise NotImplementedError("Sliding window is currently only implemented for causal masking")
+
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        expanded_attn_mask = self._expand_mask(attention_mask_2d, dtype, tgt_len=input_shape[-1]).to(
+            attention_mask_2d.device
+        )
+        expanded_4d_mask = expanded_attn_mask if causal_4d_mask is None else expanded_attn_mask + causal_4d_mask
+
+        return expanded_4d_mask
+
+    @staticmethod
+    def _make_causal_mask(
+        input_ids_shape: torch.Size,
+        dtype: torch.dtype,
+        device: torch.device,
+        past_key_values_length: int = 0,
+        sliding_window: Optional[int] = None,
+    ):
+        """
+        Make causal mask used for bi-directional self-attention.
+        """
+        bsz, tgt_len = input_ids_shape
+        mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
+        mask_cond = torch.arange(mask.size(-1), device=device)
+        mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+
+        mask = mask.to(dtype)
+
+        if past_key_values_length > 0:
+            mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
+
+        # add lower triangular sliding window mask if necessary
+        if sliding_window is not None:
+            diagonal = past_key_values_length - sliding_window + 1
+
+            context_mask = 1 - torch.triu(torch.ones_like(mask, dtype=torch.int), diagonal=diagonal)
+            mask.masked_fill_(context_mask.bool(), torch.finfo(dtype).min)
+
+        return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
+
+    @staticmethod
+    def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
+        """
+        Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
+        """
+        bsz, src_len = mask.size()
+        tgt_len = tgt_len if tgt_len is not None else src_len
+
+        expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
+
+        inverted_mask = 1.0 - expanded_mask
+
+        return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
+
+
+def _prepare_4d_causal_attention_mask(
+    attention_mask: Optional[torch.Tensor],
+    input_shape: Union[torch.Size, Tuple, List],
+    inputs_embeds: torch.Tensor,
+    past_key_values_length: int,
+    sliding_window: Optional[int] = None,
+):
+    """
+    Creates a causal 4D mask of shape `(batch_size, 1, query_length, key_value_length)` from a 2D mask of shape
+    `(batch_size, key_value_length)`
+
+    Args:
+        attention_mask (`torch.Tensor` or `None`):
+            A 2D attention mask of shape `(batch_size, key_value_length)`
+        input_shape (`tuple(int)` or `list(int)` or `torch.Size`):
+            The input shape should be a tuple that defines `(batch_size, query_length)`.
+        inputs_embeds (`torch.Tensor`):
+            The embedded inputs as a torch Tensor.
+        past_key_values_length (`int`):
+            The length of the key value cache.
+        sliding_window (`int`, *optional*):
+            If the model uses windowed attention, a sliding window should be passed.
+    """
+    attn_mask_converter = AttentionMaskConverter(is_causal=True, sliding_window=sliding_window)
+
+    key_value_length = input_shape[-1] + past_key_values_length
+
+    # 4d mask is passed through the layers
+    if attention_mask is not None:
+        attention_mask = attn_mask_converter.to_4d(
+            attention_mask, input_shape[-1], key_value_length, dtype=inputs_embeds.dtype
+        ).to(dtype=inputs_embeds.dtype)
+    else:
+        attention_mask = attn_mask_converter.to_causal_4d(
+            input_shape[0], input_shape[-1], key_value_length, dtype=inputs_embeds.dtype, device=inputs_embeds.device
+        ).to(dtype=inputs_embeds.dtype)
+
+    return attention_mask
+
+
+def _prepare_4d_attention_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
+    """
+    Creates a non-causal 4D mask of shape `(batch_size, 1, query_length, key_value_length)` from a 2D mask of shape
+    `(batch_size, key_value_length)`
+
+    Args:
+        mask (`torch.Tensor` or `None`):
+            A 2D attention mask of shape `(batch_size, key_value_length)`
+        dtype (`torch.dtype`):
+            The torch dtype the created mask shall have.
+        tgt_len (`int`):
+            The target length or query length the created mask shall have.
+    """
+    return AttentionMaskConverter._expand_mask(mask=mask, dtype=dtype, tgt_len=tgt_len)
+
+
+def _create_4d_causal_attention_mask(
+    input_shape: Union[torch.Size, Tuple, List],
+    dtype: torch.dtype,
+    device: torch.device,
+    past_key_values_length: int = 0,
+    sliding_window: Optional[int] = None,
+):
+    """
+    Creates a causal 4D mask of shape `(batch_size, 1, query_length, key_value_length)`
+
+    Args:
+        input_shape (`tuple(int)` or `list(int)` or `torch.Size`):
+            The input shape should be a tuple that defines `(batch_size, query_length)`.
+        dtype (`torch.dtype`):
+            The torch dtype the created mask shall have.
+        device (`int`):
+            The torch device the created mask shall have.
+        sliding_window (`int`, *optional*):
+            If the model uses windowed attention, a sliding window should be passed.
+    """
+    attn_mask_converter = AttentionMaskConverter(is_causal=True, sliding_window=sliding_window)
+
+    key_value_length = past_key_values_length + input_shape[-1]
+    attention_mask = attn_mask_converter.to_causal_4d(
+        input_shape[0], input_shape[-1], key_value_length, dtype=dtype, device=device
+    ).to(dtype=dtype)
+
+    return attention_mask
+
+
+def init_feature_map(feature_map: str='none', **kwargs: any):
+    """
+    Initialize query and key mapping for linear attention
+    """
+    if feature_map in [None, 'none', 'identity']:
+        return FeatureMap(**kwargs)
+    # Taylor series approximations to exp(x)
+    elif feature_map == 'taylor_exp':
+        return TaylorExp(**kwargs) 
+    else:
+        raise NotImplementedError(f'Sorry "{feature_map}" feature map not implemented.')
+        
+        
+class FeatureMap(nn.Module):
+    """
+    Parent feature map; default is identity function
+    """
+    def __init__(self, 
+                 input_dim: int,                 
+                 temp: int = None,
+                 head_dim_idx: int = -1, 
+                 eps: float = 1e-12, 
+                 **kwargs: any):
+        super().__init__()
+        self.input_dim = input_dim
+        self.head_dim_idx = head_dim_idx     
+        self.temp = 1. if temp is None else temp
+        self.eps = eps
+        
+    def forward(self, x: torch.Tensor):
+        """
+        Assume x.shape is (batch_size, n_heads, seq_len, head_dim)
+        """
+        return x
+
+    
+class TaylorExp(FeatureMap):
+    """
+    Feature map to compute 2nd-order Taylor approx. of exp(q^T k / sqrt(d))
+    """
+    def __init__(self, input_dim: int, **kwargs: any):
+        super().__init__(input_dim, **kwargs)
+        self.r2  = math.sqrt(2)
+        self.rd  = math.sqrt(self.input_dim)
+        self.rrd = math.sqrt(self.rd)
+        self.tril_indices = torch.tril_indices(self.input_dim, self.input_dim, -1)
+        
+    # Running these in parallel
+    def forward(self, x: torch.Tensor):
+        # Get 2nd-order terms (rearrange(x * x), '... m n -> ... (m n)')
+        x2 = (x.unsqueeze(-1) * x.unsqueeze(-2)).flatten(start_dim=-2) / self.r2
+        return torch.cat([torch.ones(x[..., :1].shape).to(x.device), 
+                          x / self.rrd, x2 / self.rd], dim=self.head_dim_idx)
+        
+    def forward_mem_save(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Compute f(x) s.t. f(x)^T f(x') = 1 + x^Tx' + (x^Tx')^2 / 2
+        -> Assume x.shape is (batch_size, n_heads, seq_len, head_dim)
+        """
+        # Slow but memory-saving way to compute 2nd-order terms; how do w/o outer-product first?
+        x2  = oe.contract('...m,...n->...mn', x, x) / self.input_dim
+        x2d = torch.diagonal(x2, dim1=-2, dim2=-1) / self.r2
+        x2  = x2[..., self.tril_indices[0], self.tril_indices[1]]
+        x   = torch.cat([torch.ones(x[..., :1].shape).to(x.device), 
+                         x / self.rd, x2d, x2], dim=-1)
+        return x 
+
+
+def _make_causal_mask(
+    input_ids_shape: torch.Size, dtype: torch.dtype, device: torch.device, past_key_values_length: int = 0
+):
+    warnings.warn(
+        "Calling `transformers.models.llama.modeling_llama._make_causal_mask` is deprecated and will be removed in v4.37. Use `transformers.models.llama.modeling_llama.AttentionMaskConverter._make_causal_mask"
+    )
+    return AttentionMaskConverter._make_causal_mask(
+        input_ids_shape=input_ids_shape, dtype=dtype, device=device, past_key_values_length=past_key_values_length
+    )
+
+
+def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
+    warnings.warn(
+        "Calling `transformers.models.llama.modeling_llama._prepare_4d_attention_mask` is deprecated and will be removed in v4.37. Use `transformers.modeling_attn_mask_utils.AttentionMaskConverter._prepare_4d_attention_mask"
+    )
+    return _prepare_4d_attention_mask(mask=mask, dtype=dtype, tgt_len=tgt_len)
+
+
+class LinearAttentionHedgehog(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        l_max: int = 2048,
+        feature_dim: int = 16,
+        num_key_value_heads: int = 12,
+        num_heads: int = 12,
+        feature_name: "str" = "taylor_exp",
+        eps: float = 1e-12,
+        causal: bool = True,
+        **kwargs
+    ):
+        super().__init__()
+        self.d_model = d_model
+        self.l_max = l_max
+
+        # linear attention 
+        self.feature_name = feature_name
+        self.feature_dim = feature_dim
+        self.num_key_value_heads = num_key_value_heads
+        self.num_heads = num_heads
+        self.head_dim = self.d_model // self.num_key_value_heads
+        self.causal=causal
+        feature_map_kwargs = {
+            'input_dim': self.feature_dim,
+            'head_dim_idx': -1,
+            'temp': 1.,
+            'eps': 1e-12
+        }
+        self.feature_map = init_feature_map(feature_map=self.feature_name, **feature_map_kwargs)
+        self.proj_q = nn.Linear(self.d_model, self.feature_dim * self.num_heads, bias=False)
+        self.proj_k = nn.Linear(self.d_model, self.feature_dim * self.num_heads, bias=False)
+        self.proj_v = nn.Linear(self.d_model, self.num_key_value_heads * self.head_dim, bias=False)
+        self.proj_o = nn.Linear(self.num_heads * self.head_dim, self.d_model, bias=False)
+        self.dropout = nn.Identity()
+        self.eps = eps
+
+
+    def _prepare_decoder_attention_mask(self, attention_mask, input_shape, 
+                                        inputs_embeds, past_key_values_length):
+        # create causal mask
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        combined_attention_mask = None
+        if input_shape[-1] > 1:
+            # print(f"{input_shape=}, {past_key_values_length=}")
+            combined_attention_mask = _make_causal_mask(
+                input_shape,
+                inputs_embeds.dtype,
+                device=inputs_embeds.device,
+                past_key_values_length=past_key_values_length,
+            )
+
+        if attention_mask is not None:
+            # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+            expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]).to(
+                inputs_embeds.device
+            )
+            combined_attention_mask = (
+                expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
+            )
+
+        return combined_attention_mask
+
+
+    def forward(self, hidden_states: torch.Tensor, filters: torch.Tensor=None, *args, **kwargs):
+        """
+        x (torch.Tensor): tensor of shape (b, d, l)
+        y (torch.Tensor): tensor of shape (b, d, l)
+        """
+        # hidden_states = hidden_states.transpose(1, 2)
+        b, l, _ = hidden_states.size()
+        # print(f"{b=}, {l=}")
+        q, k, v = self.proj_q(hidden_states), self.proj_k(hidden_states), self.proj_v(hidden_states)
+        # print(f"{q.shape=}, {k.shape=}, {v.shape=}")
+        # print(f"{ self.num_key_value_heads=}, {self.feature_dim=}, {self.num_heads=}, {self.head_dim=}\n")
+
+        q = q.view(b, l, self.num_heads, self.feature_dim).transpose(1, 2)
+        # print(f"{q.shape=}")
+        k = k.view(b, l, self.num_key_value_heads, self.feature_dim).transpose(1, 2)
+        # print(f"{k.shape=}")
+        v = v.view(b, l, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        # print(f"{v.shape=}")
+        
+        # Linear attention
+        q, k = self.feature_map(q), self.feature_map(k)
+        q, k, v = q.unsqueeze(-2), k.unsqueeze(-2), v.unsqueeze(-1)
+        # print(f"Post Linear Attn: {q.shape=}, {k.shape=}, {v.shape=}")
+        
+        # Mask padding tokens.
+        attention_mask = torch.ones((b, l), dtype=torch.bool, device=hidden_states.device)
+        attention_mask = self._prepare_decoder_attention_mask(attention_mask, (b, l), hidden_states, 0)
+
+        # Mask padding tokens
+        if attention_mask is not None:
+            # attention_mask.shape could be bs, 1, 1, l or (bs, 1, n_q, n_k)
+            # print(f"{attention_mask.shape=}, {k.shape=}")
+            attention_mask = attention_mask == 0  # By now attention_mask should be 0 if include, -inf if ignore
+            attention_mask = attention_mask[:, 0, -1, :][:, None, :, None, None]  # MZ 9/7; hack, but mask usually b, h, q_len, kv_len
+            # print(f"{attention_mask.shape=}, {k.shape=}")
+            k = k.masked_fill(~attention_mask.bool(), 0)
+        
+        # Compute attention
+        if self.causal:
+            y = ((q * (k * v).cumsum(dim=2)).sum(dim=-1) / 
+                 ((q * k.cumsum(dim=2)).sum(dim=-1) + self.eps))
+        else:
+            y = ((q * (k * v).sum(dim=2, keepdim=True)).sum(dim=-1) /
+                 ((q * k.sum(dim=2, keepdim=True)).sum(dim=-1) + self.eps))
+        y = rearrange(y, 'b h l d -> b l (h d)')
+        y = self.proj_o(y.to(hidden_states.dtype))
+        y = self.dropout(y)
+        return y.to(hidden_states.dtype) #.transpose(1, 2)
+
